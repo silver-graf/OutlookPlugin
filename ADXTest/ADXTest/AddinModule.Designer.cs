630b3c109d3faffe678ba1d655f8b1af12f82f9d
csharp.company <csharp.company@gmail.com>
2019-01-25 23:00:16 +0200

    init

---
+++ ADXTest/ADXTest/AddinModule.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ADXTest
+{
+    partial class AddinModule
+    {
+        /// <summary>
+        /// Required by designer
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+ 
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by designer support - do not modify
+        /// the following method
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.adxRibbonTab1 = new AddinExpress.MSO.ADXRibbonTab(this.components);
+            this.adxRibbonGroup1 = new AddinExpress.MSO.ADXRibbonGroup(this.components);
+            this.adxRibbonButtonGroup1 = new AddinExpress.MSO.ADXRibbonButtonGroup(this.components);
+            this.adxRibbonButton1 = new AddinExpress.MSO.ADXRibbonButton(this.components);
+            this.adxRibbonButton2 = new AddinExpress.MSO.ADXRibbonButton(this.components);
+            // 
+            // adxRibbonTab1
+            // 
+            this.adxRibbonTab1.Caption = "adxRibbonTab1";
+            this.adxRibbonTab1.Controls.Add(this.adxRibbonGroup1);
+            this.adxRibbonTab1.Id = "adxRibbonTab_57cc638c2b9247a0ac8734ea32d12fee";
+            this.adxRibbonTab1.Ribbons = ((AddinExpress.MSO.ADXRibbons)(((((((((((((((((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead | AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestSend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookContact) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookJournal) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookTask) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookReport) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCounterPropose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookRSS) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrPowerPointPresentation) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrInfoPathPrintPreview) 
+            | AddinExpress.MSO.ADXRibbons.msrProjectProject) 
+            | AddinExpress.MSO.ADXRibbons.msrOneNote)));
+            // 
+            // adxRibbonGroup1
+            // 
+            this.adxRibbonGroup1.Caption = "adxRibbonGroup1";
+            this.adxRibbonGroup1.Controls.Add(this.adxRibbonButtonGroup1);
+            this.adxRibbonGroup1.Id = "adxRibbonGroup_130dae90d20f4edcae480a8f82fd6166";
+            this.adxRibbonGroup1.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxRibbonGroup1.Ribbons = ((AddinExpress.MSO.ADXRibbons)(((((((((((((((((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead | AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestSend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookContact) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookJournal) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookTask) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookReport) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCounterPropose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookRSS) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrPowerPointPresentation) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrInfoPathPrintPreview) 
+            | AddinExpress.MSO.ADXRibbons.msrProjectProject) 
+            | AddinExpress.MSO.ADXRibbons.msrOneNote)));
+            // 
+            // adxRibbonButtonGroup1
+            // 
+            this.adxRibbonButtonGroup1.Controls.Add(this.adxRibbonButton1);
+            this.adxRibbonButtonGroup1.Controls.Add(this.adxRibbonButton2);
+            this.adxRibbonButtonGroup1.Id = "adxRibbonButtonGroup_46872dadc68e4db495303cb3f12d20e9";
+            this.adxRibbonButtonGroup1.Ribbons = ((AddinExpress.MSO.ADXRibbons)(((((((((((((((((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead | AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestSend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookContact) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookJournal) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookTask) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookReport) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCounterPropose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookRSS) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrPowerPointPresentation) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrInfoPathPrintPreview) 
+            | AddinExpress.MSO.ADXRibbons.msrProjectProject) 
+            | AddinExpress.MSO.ADXRibbons.msrOneNote)));
+            // 
+            // adxRibbonButton1
+            // 
+            this.adxRibbonButton1.Caption = "adxRibbonButton1";
+            this.adxRibbonButton1.Id = "adxRibbonButton_4a261c021df84f6c994e2216328622b5";
+            this.adxRibbonButton1.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxRibbonButton1.Ribbons = ((AddinExpress.MSO.ADXRibbons)(((((((((((((((((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead | AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestSend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookContact) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookJournal) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookTask) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookReport) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCounterPropose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookRSS) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrPowerPointPresentation) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrInfoPathPrintPreview) 
+            | AddinExpress.MSO.ADXRibbons.msrProjectProject) 
+            | AddinExpress.MSO.ADXRibbons.msrOneNote)));
+            // 
+            // adxRibbonButton2
+            // 
+            this.adxRibbonButton2.Caption = "adxRibbonButton2";
+            this.adxRibbonButton2.Id = "adxRibbonButton_13275af4053f4ae88b5f24c1810cd486";
+            this.adxRibbonButton2.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.adxRibbonButton2.Ribbons = ((AddinExpress.MSO.ADXRibbons)(((((((((((((((((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead | AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestSend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookContact) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookJournal) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookTask) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookReport) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResend) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookResponseCounterPropose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookRSS) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookPostCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrPowerPointPresentation) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookSMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead) 
+            | AddinExpress.MSO.ADXRibbons.msrOutlookMMSCompose) 
+            | AddinExpress.MSO.ADXRibbons.msrInfoPathPrintPreview) 
+            | AddinExpress.MSO.ADXRibbons.msrProjectProject) 
+            | AddinExpress.MSO.ADXRibbons.msrOneNote)));
+            // 
+            // AddinModule
+            // 
+            this.AddinName = "ADXTest";
+            this.SupportedApps = AddinExpress.MSO.ADXOfficeHostApp.ohaOutlook;
+
+        }
+        #endregion
+
+        private AddinExpress.MSO.ADXRibbonTab adxRibbonTab1;
+        private AddinExpress.MSO.ADXRibbonGroup adxRibbonGroup1;
+        private AddinExpress.MSO.ADXRibbonButtonGroup adxRibbonButtonGroup1;
+        private AddinExpress.MSO.ADXRibbonButton adxRibbonButton1;
+        private AddinExpress.MSO.ADXRibbonButton adxRibbonButton2;
+    }
+}
+
---
+++ ADXTest/ADXTest/AddinModule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+
+namespace ADXTest
+{
+    /// <summary>
+    ///   Add-in Express Add-in Module
+    /// </summary>
+    [GuidAttribute("A0218000-6FEA-4C8E-BEB4-08615ED292FA"), ProgId("ADXTest.AddinModule")]
+    public partial class AddinModule : AddinExpress.MSO.ADXAddinModule
+    {
+        public AddinModule()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+            // Please add any initialization code to the AddinInitialize event handler
+        }
+ 
+        #region Add-in Express automatic code
+ 
+        // Required by Add-in Express - do not modify
+        // the methods within this region
+ 
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+ 
+        [ComRegisterFunctionAttribute]
+        public static void AddinRegister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXRegister(t);
+        }
+ 
+        [ComUnregisterFunctionAttribute]
+        public static void AddinUnregister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXUnregister(t);
+        }
+ 
+        public override void UninstallControls()
+        {
+            base.UninstallControls();
+        }
+
+        #endregion
+
+        public static new AddinModule CurrentInstance 
+        {
+            get
+            {
+                return AddinExpress.MSO.ADXAddinModule.CurrentInstance as AddinModule;
+            }
+        }
+
+        public Outlook._Application OutlookApp
+        {
+            get
+            {
+                return (HostApplication as Outlook._Application);
+            }
+        }
+
+    }
+}
+
